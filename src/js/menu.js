8372ecdf5120ada8252bdd2b57647aa862bbafc8
Jhony Freitas <jhony00._@hotmail.com>
2022-07-11 21:51:09 -0300

    Rotação do menu hamburger

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,17 @@
 let menu = document.querySelector(".menu");
 let menuHamburger = document.querySelector(".menu-hamburger");
+let rotacaoMenu = document.querySelector(".hamburger");
 let statusAtivo = false;
 
 
 menuHamburger.addEventListener('click', function(){
     if (statusAtivo) {
         menu.classList.remove("versao-hamburger");
+        rotacaoMenu.classList.remove("ativo");
         statusAtivo = false;
     }else{
         menu.classList.add("versao-hamburger");        
+        rotacaoMenu.classList.add("ativo");
         statusAtivo = true;
     };
 });
